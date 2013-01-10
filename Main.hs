fef187a48e051dd4db44ffb6f233fa0475151c46
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2013-01-10 14:06:06 +0200

    Enable 21 now that it compiles

--- Main.hs
+++ Main.hs
@@ -20,7 +20,7 @@ import qualified Evolution_17_Interpretive
 -- import qualified Evolution_18_Static
 import qualified Evolution_19_BeginningGraduate
 import qualified Evolution_20_Origamist
--- import qualified Evolution_21_CartesianallyInclined
+import qualified Evolution_21_CartesianallyInclined
 import qualified Evolution_22_PHD
 import qualified Evolution_23_PostDoc
 import qualified Evolution_24_TenuredProfessor
@@ -48,6 +48,7 @@ main = defaultMain [ bgroup "fact" [
        -- , bench "Static"                  $ nf Evolution_18_Static.fac                  10000
        , bench "BeginningGraduate"       $ nf Evolution_19_BeginningGraduate.fac       10000
        , bench "Origamist"               $ nf Evolution_20_Origamist.fac               10
+       , bench "CartesianallyInclined"   $ nf Evolution_21_CartesianallyInclined.fac   10
        , bench "PHD"                     $ nf Evolution_22_PHD.fac                     1000
        , bench "PostDoc"                 $ nf Evolution_23_PostDoc.fac                 1000
        , bench "TenuredProfessor"        $ nf Evolution_24_TenuredProfessor.fac        10
